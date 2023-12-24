e8c7b7fd56b261696dacf2475ca4e8d88cf84ca3
hywax <a.hywax@gmail.com>
2023-12-24 20:25:30 +0500

    chore(skeleton): base icon component

---
+++ components/service/Icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <img src="http://localhost:8090/icons/authelia.png" alt="" class="block overflow-hidden bg-contain w-16 h-16 rounded-2xl border border-black/5 shadow-sm" style="">
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  type: 'solid' | 'gradient'
+  value: string
+}
+
+defineProps<Props>()
+</script>
