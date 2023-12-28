c8c3ef97605cfbb31cef2201efb00c9372415e44
hywax <a.hywax@gmail.com>
2023-12-29 01:05:48 +0500

    refactor(skeleton): service group

--- components/Group.vue
+++ components/Group.vue
@@ -4,17 +4,19 @@
       {{ title }}
     </h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 lg:gap-2 lg:gap-y-4">
-      <template v-for="(item, key) in items" :key="key">
-        <ServiceItem v-bind="item" />
+      <template v-for="item in items" :key="item.id">
+        <Item v-bind="item" />
       </template>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { BaseService } from '~/types'
+
 export interface Props {
   title: string
-  items: any[]
+  items: BaseService[]
 }
 
 defineProps<Props>()
