ff9b0d64f9235b08b6cd20c0f0903f410f736849
hywax <a.hywax@gmail.com>
2023-12-28 18:50:10 +0500

    chore(nuxt): disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  ssr: false,
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt'
