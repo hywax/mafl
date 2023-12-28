a5ceff77b3e3ba5717389f91bd606d6a1cd19e71
hywax <a.hywax@gmail.com>
2023-12-29 01:02:01 +0500

    chore(skeleton): define global message component

---
+++ components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="min-h-screen flex flex-grow items-center justify-center">
+    <div class="rounded-lg bg-white p-8 text-center shadow-xl">
+      <h1 class="mb-4 text-2xl font-light">
+        {{ title }}
+      </h1>
+      <div class="text-sm">
+        <slot name="text">
+          {{ text }}
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string
+  text?: string
+}
+
+defineProps<Props>()
+</script>
