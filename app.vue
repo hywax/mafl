6a076880468e06b4f46156a995e24baade54b281
hywax <a.hywax@gmail.com>
2023-12-30 15:58:52 +0500

    feat(lang): load from config

--- app.vue
+++ app.vue
@@ -7,8 +7,10 @@
 <script setup lang="ts">
 const siteConfig = useSiteConfig()
 const colorMode = useColorMode()
+const { locale } = useI18n()
 
 colorMode.preference = siteConfig.theme || 'system'
+locale.value = siteConfig.lang
 
 useHead({
   title: siteConfig.title,
