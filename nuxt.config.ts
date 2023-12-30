23e03801addff782bd06b1481b4bb8f06eb33f09
hywax <a.hywax@gmail.com>
2023-12-30 15:57:31 +0500

    feat(lang): add module i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,34 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
+    '@nuxtjs/i18n',
     '@vueuse/nuxt',
     'nuxt-site-config',
   ],
   site: {
     title: 'Mafl',
+    lang: 'en',
     theme: 'system',
     services: [],
+  },
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English',
+        file: 'en-US.json',
+      },
+      {
+        code: 'ru',
+        iso: 'ru-RU',
+        name: 'Русский',
+        file: 'ru-RU.json',
+      },
+    ],
+    strategy: 'no_prefix',
+    langDir: 'locales',
+    defaultLocale: 'en',
   },
   tailwindcss: {
     cssPath: '~/assets/style/tailwind.css',
