44da7ba86c7bc91206a64315db2902be447e05cf
hywax <a.hywax@gmail.com>
2023-12-30 14:42:31 +0500

    chore: add padding axis X

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen">
-    <div class="max-w-screen-2xl mx-auto xl:flex-row">
+    <div class="max-w-screen-2xl mx-auto xl:flex-row px-4">
       <slot />
     </div>
   </div>
