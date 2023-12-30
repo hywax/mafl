65e33a77e256650215d8c7fcc17b818a70f5fae2
hywax <a.hywax@gmail.com>
2023-12-30 17:33:02 +0500

    chore: move overrides https://github.com/hywax/mafl/pull/4

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  vue: true,
   typescript: true,
   ignores: [
     '.output',
     'dist',
     'node_modules',
   ],
-  overrides: {
-    vue: {
+  vue: {
+    overrides: {
       'vue/block-order': ['error', {
         order: ['template', 'script', 'style'],
       }],
