d301ff662271dc5519a25df2158cad060044ec2a
hywax <a.hywax@gmail.com>
2023-12-31 17:23:37 +0500

    feat(update-checker): check new versions

---
+++ server/plugins/0.update-checker.ts
@@ -0,0 +1,20 @@
+import currentPackage from '~/package.json'
+
+export default defineNitroPlugin(async () => {
+  try {
+    const latestPackage = await $fetch<typeof currentPackage>('https://raw.githubusercontent.com/hywax/mafl/main/package.json', {
+      parseResponse: (json) => JSON.parse(json)
+    })
+
+    const parseVersion = (version: string): number => parseInt(version.replace(/\./g, ''), 10);
+    const difference = parseVersion(latestPackage.version) - parseVersion(currentPackage.version)
+
+    if (difference > 0) {
+      return console.log(`⚠️ update available: ${latestPackage.version}`)
+    }
+
+    console.log('✅ mafl is up-to-date')
+  } catch (_) {
+    console.log('❌ failed to check for an update')
+  }
+})
