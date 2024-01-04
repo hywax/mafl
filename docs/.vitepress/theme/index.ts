8913533940d5b7eabbf9f759d2fbb03dcfeead4b
hywax <a.hywax@gmail.com>
2024-01-05 00:37:01 +0500

    docs: create preview service

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,11 @@
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import PreviewService from '../components/preview-service.vue'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('PreviewService', PreviewService)
+  },
+} satisfies Theme
