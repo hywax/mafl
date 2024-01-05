6094ba035e267c7fe2e891345aa28fe798b96bce
hywax <a.hywax@gmail.com>
2024-01-05 20:33:23 +0500

    chore: redirect to error page

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,18 @@
 <template>
-  <template v-if="$services.length">
-    <Group
-      v-for="(group, key) in $services"
-      :key="key"
-      v-bind="group"
-    />
-  </template>
-  <Message v-else :title="$t('service.not-found.title')">
-    <template #text>
-      <p>
-        {{ $t('service.not-found.description') }}
-      </p>
-      <p class="mt-1">
-        <a href="#" class="text-sky-500 hover:text-sky-800">{{ $t('service.not-found.action') }}</a>
-      </p>
-    </template>
-  </Message>
+  <Group
+    v-for="(group, key) in $services"
+    :key="key"
+    v-bind="group"
+  />
   <Update v-if="$settings.checkUpdates" />
 </template>
 
 <script setup lang="ts">
 const { $services, $settings } = useNuxtApp()
+
+if ($settings.error) {
+  throw createError({
+    message: $settings.error,
+  })
+}
 </script>
