35c88c1b38ad5912c345d2aae9c05147a47620bd
hywax <a.hywax@gmail.com>
2024-01-21 00:41:42 +0500

    feat: dir and lang to html attrs

--- app.vue
+++ app.vue
@@ -13,8 +13,17 @@ const { locale } = useI18n()
 colorMode.preference = $settings.theme || 'system'
 locale.value = $settings.lang
 
+const i18nHead = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: 'id',
+})
+
 useHead({
   title: $settings.title,
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs?.lang,
+    dir: i18nHead.value.htmlAttrs?.dir,
+  },
   bodyAttrs: {
     class: 'relative',
   },
