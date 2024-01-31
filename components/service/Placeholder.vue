05cb0ea21b1dd7ccf5e5dbcfbaa283411dca9a7e
hywax <me@hywax.space>
2024-01-31 18:12:01 +0500

    chore: change placeholder background color

--- components/service/Placeholder.vue
+++ components/service/Placeholder.vue
@@ -2,15 +2,15 @@
   <div class="p-4 flex gap-4">
     <div class="flex-shrink-0 flex">
       <div class="self-center w-16 h-16 overflow-hidden rounded-2xl border border-black/5 animate-pulse">
-        <div class="bg-fg-dimmed h-full w-full" />
+        <div class="bg-fg/5 dark:bg-fg/10 h-full w-full" />
       </div>
     </div>
     <div>
       <h3 class="animate-pulse text-lg mt-1">
-        <span class="inline-block min-h-[1em] w-[110px] flex-auto cursor-wait bg-fg-dimmed align-middle opacity-50 rounded" />
+        <span class="inline-block min-h-[1em] w-[110px] flex-auto cursor-wait bg-fg/5 dark:bg-fg/10 align-middle opacity-50 rounded" />
       </h3>
       <p class="animate-pulse text-sm text-black/50">
-        <span class="inline-block min-h-[1em] w-[160px] flex-auto cursor-wait bg-fg-dimmed align-middle opacity-50 rounded" />
+        <span class="inline-block min-h-[1em] w-[160px] flex-auto cursor-wait bg-fg/5 dark:bg-fg/10 align-middle opacity-50 rounded" />
       </p>
     </div>
   </div>
