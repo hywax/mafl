22d343032066dcec9f2b997b6ab30365bd28954b
Bot <158096250+hywax-assistant@users.noreply.github.com>
2024-01-31 16:09:23 +0500

    chore(deps): update all non-major dependencies (#35)
    * chore(deps): update all non-major dependencies
    
    * chore: remove logger fancy
    
    * chore: optimize use args create logger
    
    ---------
    
    Co-authored-by: hywax <me@hywax.space>

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -4,5 +4,5 @@ import type { ConsolaOptions } from 'consola'
 export const logger = consola
 
 export function useLogger(tag?: string, options: Partial<ConsolaOptions> = {}) {
-  return tag ? createConsola({ ...options, fancy: true }).withTag(tag) : logger
+  return tag ? createConsola(options).withTag(tag) : logger
 }
