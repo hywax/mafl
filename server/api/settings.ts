e38a7a519d26f3a201c026d3c993faab7218d554
hywax <me@hywax.space>
2024-02-01 15:57:55 +0500

    chore: provide safely config

--- server/api/settings.ts
+++ server/api/settings.ts
@@ -2,6 +2,14 @@ import type { CompleteConfig } from '~/types'
 
 export default defineEventHandler(async () => {
   const storage = useStorage('main')
+  const config = await storage.getItem<CompleteConfig>('config')
 
-  return await storage.getItem<CompleteConfig>('config')
+  if (!config) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Bad loading config',
+    })
+  }
+
+  return extractSafelyConfig(config)
 })
